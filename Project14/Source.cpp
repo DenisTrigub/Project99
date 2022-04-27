1890835d52276231c02cd292ce50923569a1b78a
User <User@DESKTOP-802SV85>
2022-04-27 19:47:05 +0300

    сделаю завтра

--- Project14/Source.cpp
+++ Project14/Source.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+int func(int arr[], int* size)
+{
+	int min, nul, max;
+	for (int i = 0; i < *size; i++)
+	{
+		arr = new int[i];
+		arr[i] = -10 + rand() % 50;
+	    cout << " " << arr[i];
+		cout << endl;
+
+		if (arr[i] < 0)
+		{
+			min == arr[i];
+		}
+		cout << endl;
+		if (arr[i] == 0)
+		{
+			nul = arr[i];
+		}
+		cout << endl;
+		if (arr[i] > 0)
+		{
+			max = arr[i];
+		}
+		
+	}
+	cout << "менише 0 " << min << " равно 0 " << nul << "больше 0 " << max;
+	return *arr;
+}
 int main()
 {
+	srand(time(0));
+	setlocale(0,"");
+	int* size = new int(6);
+	int* arr = new int [*size];
+	cout << "введите размер массива: ";
+	cin >> *size;
+	func(arr, size);
 
 }
