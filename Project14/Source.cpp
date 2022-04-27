9fbb2f15d32cd63492fff846fb35c5b3cd7baab7
User <User@DESKTOP-802SV85>
2022-04-27 19:10:11 +0300

    task 1 done

--- Project14/Source.cpp
+++ Project14/Source.cpp
@@ -2,7 +2,38 @@
 
 using namespace std;
 
+void func(int* arr, int size, int* summ, int* mult)
+{
+    for (int i = 0; i < size; i++)
+    {
+        *summ += *(arr + i);
+ 
+    }
+    cout << "Sum: " << *summ << endl;
+    for (int j = 0; j < size; j++)
+    {
+        *mult *= (*arr + j);
+    }
+    cout << "Mult: " << *mult << endl;
+}
+
 int main()
 {
+    srand(time(0));
+    const int size = 5;
+    int arr[size];
+    int summ = 0, multi = 1;
+    int* psumm = &summ;
+    int* pmulti = &multi;
+  
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100;
+        cout << " " << arr[i];
+    }
+    cout << endl;
+    int* pa = &arr[0];
+    func(pa, size, psumm, pmulti);
 
+    return 0;
 }
