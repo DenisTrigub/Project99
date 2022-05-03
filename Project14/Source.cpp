530a61269f80198bb14b591ea6f95b1cc39336a3
User <User@DESKTOP-802SV85>
2022-05-03 21:02:59 +0300

    task 2 done

--- Project14/Source.cpp
+++ Project14/Source.cpp
@@ -2,43 +2,27 @@
 
 using namespace std;
 
-int func(int arr[], int* size)
-{
-	int min, nul, max;
-	for (int i = 0; i < *size; i++)
-	{
-		arr = new int[i];
-		arr[i] = -10 + rand() % 50;
-	    cout << " " << arr[i];
-		cout << endl;
 
-		if (arr[i] < 0)
-		{
-			min == arr[i];
-		}
-		cout << endl;
-		if (arr[i] == 0)
-		{
-			nul = arr[i];
-		}
-		cout << endl;
-		if (arr[i] > 0)
-		{
-			max = arr[i];
-		}
-		
-	}
-	cout << "менише 0 " << min << " равно 0 " << nul << "больше 0 " << max;
-	return *arr;
-}
 int main()
 {
-	srand(time(0));
-	setlocale(0,"");
-	int* size = new int(6);
-	int* arr = new int [*size];
-	cout << "введите размер массива: ";
-	cin >> *size;
-	func(arr, size);
+    srand(time(0));
+    setlocale(0,"");
+    int n = 9;
+    cout << " введите размер масива";
+    cin >> n;
+    int* a = new int[n];
+    int pos = 0, neg = 0, null = 0;
 
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 21 - 10;
+        if (a[i] > 0) pos++;
+        else if (a[i] < 0) neg++;
+        else null++;
+        cout << a[i] << " ";
+    }
+    cout << "\nпозетивные=" << pos << "  негативные=" << neg << "  нули=" << null << "\n";
+   
 }
+
+
