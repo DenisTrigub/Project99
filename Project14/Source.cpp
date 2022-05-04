87bb90b21064856b6b784fa5c79163dca53ad617
User <User@DESKTOP-802SV85>
2022-05-04 17:20:59 +0300

    task 3 done

--- Project14/Source.cpp
+++ Project14/Source.cpp
@@ -1,8 +1,45 @@
 #include<iostream>
 
 using namespace std;
-
-int main()
+#include<iostream>
+using namespace std;
+int* proverca(int* A, int* B, int size1, int size2)
 {
+    int* ptr;
+    int i = 0;
+    for (i = 0; i < size1; i++)
+    {
+        int j = i, k = 0;
+        while (j < size1 && k < size2 && B[k] == A[j])
+        {
+            j++;
+            k++;
+        }
 
+        if (k == size2)
+            return ptr = A + i;
+    }
+    return 0; 
+}
+int main() {
+    setlocale(LC_ALL, "rus");
+    int size1, size2;
+    cout << "Введите размер массива A: ";
+    cin >> size1;
+    int* A = new int[size1 + 1];
+    cout << "Заполните массив: ";
+    for (int i = 0; i < size1; i++)
+        cin >> A[i];
+    cout << "Введите размер массива B: ";
+    cin >> size2;
+    int* B = new int[size2];
+    cout << "Заполните массив: ";
+    for (int i = 0; i < size2; i++)
+        cin >> B[i];
+
+
+    cout << "Элемент должен стоять на позиции " << *proverca(A, B, size1, size2) << endl;
+
+
+    return 0;
 }
